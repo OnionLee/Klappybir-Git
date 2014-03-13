6b520f0977436d762acdf28d95af26d986a247f4
LeeKangHee <rkdgml6332@gmail.com>
2014-03-13 20:33:01 +0900

    2ndCommit
    이연수 자살해

--- Classes/KCNetworkMgr.cpp
+++ Classes/KCNetworkMgr.cpp
@@ -1,11 +1,78 @@
 #include "KCNetworkMgr.h"
 
-
+//php 파일 목록
+//KlappyGetHighScore.php
+//KlappyGetHighScoreTable.php
+//KlappyLogin.php
+//KlappyRegister.php
+//KlappySaveHighScore.php
 KCNetworkMgr::KCNetworkMgr()
 {
+	m_sServerPath = "http://teamleaf.hubweb.net/KlappyBird/";
+	
 }
 
 
 KCNetworkMgr::~KCNetworkMgr()
 {
+
+}
+
+
+void KCNetworkMgr::KlappyGetHighScoreTable()
+{
+
+}
+
+void KCNetworkMgr::KlappyLogin(const char * id, const char * passworld)
+{
+	std::string sURL = m_sServerPath + "?id=" + id + "&pass=" + passworld;
+	m_pRequest = new HttpRequest;
+	m_pRequest->setUrl(sURL.c_str());
+	m_pRequest->setResponseCallback(this, callfuncND_selector(KCNetworkMgr::CallKlappyLogin));
+}
+
+void KCNetworkMgr::KlappyRegister(const char * id, const char * passworld)
+{
+
+}
+
+void KCNetworkMgr::KlappySetHighScore(const char * score)
+{
+
+}
+
+void KCNetworkMgr::KlappyGetHiqghScore(const char * score)
+{
+
+}
+
+void KCNetworkMgr::NetWork_Update(float dt)
+{
+
+}
+
+void KCNetworkMgr::CallKlappyLogin(Node * p_Sender, void * data)
+{
+
+}
+
+void KCNetworkMgr::CallKlappyRegister(Node * p_Sender, void * data)
+{
+
+}
+
+void KCNetworkMgr::CallKlappySetHighScore(Node * p_Sender, void * data)
+{
+
+}
+
+void KCNetworkMgr::CallKlappyGetHiqghScore(Node * p_Sender, void * data)
+{
+
+}
+
+void KCNetworkMgr::CallKlappyGetHighScoreTable(Node * p_Sender, void * data)
+{
+
 }
--- Classes/KCNetworkMgr.h
+++ Classes/KCNetworkMgr.h
@@ -1,7 +1,49 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "cocos2d.h"
+#include "extensions/cocos-ext.h"
+#include "network/HttpClient.h"
+
+using namespace cocos2d::extension;
+using namespace cocos2d::network;
+using namespace cocos2d;
+
+
+struct sUserData{
+	char m_cScore[16];
+	char m_cName[32];
+};
+
 class KCNetworkMgr
 {
 public:
+	std::string m_sServerPath;
+
+	void KlappyLogin(const char * id, const char * passworld);
+	void KlappyRegister(const char * id, const char * passworld);
+	void KlappySetHighScore(const char * score);
+	void KlappyGetHiqghScore(const char * score);
+	void KlappyGetHighScoreTable();
+
+	void CallKlappyLogin(Node * p_Sender, void * data);
+	void CallKlappyRegister(Node * p_Sender, void * data);
+	void CallKlappySetHighScore(Node * p_Sender, void * data);
+	void CallKlappyGetHiqghScore(Node * p_Sender, void * data);
+	void CallKlappyGetHighScoreTable(Node * p_Sender, void * data);
+
+
+
+	Node * GetTarget() const { return m_pTarget; }
+	void SetTarget(Node * val) { m_pTarget = val; }
+
+	void NetWork_Update(float dt);
+
+private:
+
+	HttpRequest * m_pRequest;
+	Node * m_pTarget;
+
 	KCNetworkMgr();
 	~KCNetworkMgr();
 };
